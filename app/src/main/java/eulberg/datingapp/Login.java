547ee0718f638f250e71d643e5a34bd4fa33e300
Joshua <joshua-eulberg@web.de>
2018-11-14 10:24:48 +0100

    Buttons

--- app/src/main/java/eulberg/datingapp/Login.java
+++ app/src/main/java/eulberg/datingapp/Login.java
@@ -1,9 +1,13 @@
 package eulberg.datingapp;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class Login extends AppCompatActivity {
@@ -12,10 +16,32 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         RelativeLayout login = findViewById(R.id.login);
         AnimationDrawable animationDrawable = (AnimationDrawable) login.getBackground();
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
+
+        //Login Button
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Home.class);
+                startActivity(intent);
+            }
+        });
+
+        //Register Button
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
