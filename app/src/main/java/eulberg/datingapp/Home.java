10501fe6dfe69fe833e94c296b9ebd0024fccec7
Joshua <joshua-eulberg@web.de>
2018-12-02 16:20:09 +0100

    Navigation bar

--- app/src/main/java/eulberg/datingapp/Home.java
+++ app/src/main/java/eulberg/datingapp/Home.java
@@ -1,7 +1,11 @@
 package eulberg.datingapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //Navigation bar
+        BottomNavigationView nav = findViewById(R.id.navigation_bar);
+        nav.setOnNavigationItemSelectedListener(navListener);
+        //Default fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DiscoverFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_chat:
+                    selectedFragment =  new ChatFragment();
+                    break;
+                case R.id.nav_discover:
+                    selectedFragment =  new DiscoverFragment();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment =  new ProfileFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
