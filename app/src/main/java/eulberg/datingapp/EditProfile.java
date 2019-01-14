0661a54095110e975f9c0800b53dd355a5f0d06b
Joshua <joshua-eulberg@web.de>
2019-01-14 17:17:33 +0100

    profile

---
+++ app/src/main/java/eulberg/datingapp/EditProfile.java
@@ -0,0 +1,17 @@
+package eulberg.datingapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditProfile extends AppCompatActivity {
+
+    private static final String TAG = "EditProfile";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+    }
+
+}
