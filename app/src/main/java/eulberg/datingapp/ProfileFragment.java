66215c11d0f5038b6ff1335cb5959ea018a65bd9
devcluchi <luis.boehme@gmx.net>
2019-01-16 23:21:35 +0100

    EditProfile Button hinzugefügt

--- app/src/main/java/eulberg/datingapp/ProfileFragment.java
+++ app/src/main/java/eulberg/datingapp/ProfileFragment.java
@@ -1,18 +1,37 @@
 package eulberg.datingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+       return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    //TODO ProfileFragment schließt sobald onCreate drin ist
+    /*@Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button edit = Objects.requireNonNull(getView()).findViewById(R.id.editButton);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),EditProfile.class);
+                startActivity(intent);
+            }
+        });
+    }*/
 }
