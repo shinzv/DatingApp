e863da8beca6ec502122db8469eb4989088f0417
tonzot <h134679258456>
2019-01-19 13:02:57 +0100

    Profile Edit Button

--- app/src/main/java/eulberg/datingapp/ProfileFragment.java
+++ app/src/main/java/eulberg/datingapp/ProfileFragment.java
@@ -1,24 +1,19 @@
 package eulberg.datingapp;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
-    private Button editButton;
+    private ImageButton editButton;
 
     private ImageView profilePicture;
 
@@ -26,6 +21,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        return inflater.inflate(R.layout.fragment_profile, container, false);
+
     }
 
     //TODO ProfileFragment schließt sobald onCreate drin ist
@@ -37,10 +33,13 @@ public class ProfileFragment extends Fragment {
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ProfileFragment.this, EditProfile.class);
-                startActivity(intent);
+                start();
             }
         });
+    }
+
+    public void start(){
+        startActivity(new Intent(getActivity(), EditProfile.class));
     }
 
 }
