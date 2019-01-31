dd7514ec2af98388a7e8c132b316bc61e4290ef7
Joshua <joshua-eulberg@web.de>
2019-01-31 19:12:41 +0100

    EditProfile UI

--- app/src/main/java/eulberg/datingapp/EditProfile.java
+++ app/src/main/java/eulberg/datingapp/EditProfile.java
@@ -1,8 +1,11 @@
 package eulberg.datingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -12,6 +15,16 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
+
+        View layoutTop = findViewById( R.id.rel_top );
+        ImageButton backButton = layoutTop.findViewById(R.id.backArrow);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditProfile.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
