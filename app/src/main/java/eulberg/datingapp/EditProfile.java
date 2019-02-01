6cd72eb0c0ed1c8abbdbe6c0a8100f23ad6c3f0c
tonzot <h134679258456>
2019-02-01 15:11:07 +0100

    Edit Profile: SharedPreferences -> saving data locally

--- app/src/main/java/eulberg/datingapp/EditProfile.java
+++ app/src/main/java/eulberg/datingapp/EditProfile.java
@@ -1,28 +1,62 @@
 package eulberg.datingapp;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class EditProfile extends AppCompatActivity {
 
-    private static final String TAG = "EditProfile";
+    private EditText nameEditText, ageEditText, descriptionEditText, emailEditText, phoneNumberEditText;
+    private FloatingActionButton saveDataFab;
+
+    //SharedPreferences
+    public static final String SHARED_PREFS = "SharedPrefs";
+
+    public static final String name = "Name";
+    public static final String age = "Age";
+    public static final String description = "Description";
+    public static final String email = "Email";
+    public static final String phoneNumber = "Phone number";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+
+        //Initialize all views
+        nameEditText = findViewById(R.id.username);
+        ageEditText = findViewById(R.id.age);
+        descriptionEditText = findViewById(R.id.description);
+        emailEditText = findViewById(R.id.email);
+        phoneNumberEditText = findViewById(R.id.phoneNumber);
+        saveDataFab = findViewById(R.id.save_data);
+
+        saveDataFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         //Add back button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loadData();
     }
 
     @Override
@@ -34,6 +68,40 @@ public class EditProfile extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Lädt die Daten aus der abgespeicherten SharedPreference Datei.
+     */
+    private void loadData(){
+        //Mode private bedeutet, dass keine andere App auf unsere SharedPreferences zugreifen darf.
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        //Second argument is the default value(Will be set if nothing has been found.)
+        nameEditText.setText(sharedPreferences.getString(name, ""));
+        ageEditText.setText(sharedPreferences.getString(age, ""));
+        descriptionEditText.setText(sharedPreferences.getString(description, ""));
+        emailEditText.setText(sharedPreferences.getString(email, ""));
+        phoneNumberEditText.setText(sharedPreferences.getString(phoneNumber, ""));
+
+    }
+
+    /**
+     * Speichert die daten online und offline ab.
+     */
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //Save variables.
+        editor.putString(name, nameEditText.getText().toString());
+        editor.putString(age, ageEditText.getText().toString());
+        editor.putString(description, descriptionEditText.getText().toString());
+        editor.putString(email, emailEditText.getText().toString());
+        editor.putString(phoneNumber, phoneNumberEditText.getText().toString());
+        //Writes the data synchronouslly(commit()) -> may bring ui rendering to a break, apply() -> still lets the ui render but is asynchronous
+        editor.apply();
+        Toast.makeText(EditProfile.this, "SAVED", Toast.LENGTH_SHORT).show();
+
     }
 
 }
