42675da8460988d9ba4ffce02b193222c73ea89e
devcluchi <luis.boehme@gmx.net>
2019-02-03 16:21:36 +0100

    ProfileSettings done

--- app/src/main/java/eulberg/datingapp/ProfileSettings.java
+++ app/src/main/java/eulberg/datingapp/ProfileSettings.java
@@ -2,19 +2,12 @@ package eulberg.datingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -55,43 +48,16 @@ public class ProfileSettings extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                /*StorageReference ref = storageReference.child("ProfilePictures/" + mAuth.getCurrentUser().getUid());
-                ref.delete();*/
-                DatabaseReference userDBRef = databaseReference.child("users").child(mAuth.getCurrentUser().getUid()).getRef();
-                userDBRef.removeValue();
-                startActivity(new Intent(ProfileSettings.this, Login.class));
-                /*
+                storageReference.child("ProfilePictures/" + mAuth.getCurrentUser().getUid()).delete();
+                databaseReference.child("users").child(mAuth.getCurrentUser().getUid()).removeValue();
+                databaseReference.child("user_settings").child(mAuth.getCurrentUser().getUid()).removeValue();
                 //Entfernt den User aus der Authentication Liste
                 mAuth.getCurrentUser().delete();
                 mAuth.signOut();
                 startActivity(new Intent(ProfileSettings.this, Login.class));
-                */
-
             }
         });
 
 
     }
-
-
-
-    /*private void signIn(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in erfolgreich
-                            Log.d(TAG, "sign in erfolgreich");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Intent intent = new Intent(Login.this,Home.class);
-                            startActivity(intent);
-                        } else {
-                            //Sign in fehlgeschlagen
-                            Log.w(TAG, "sign in fehlgeschlagen", task.getException());
-                            Toast.makeText(Login.this, "Anmeldung fehlgeschlagen", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }*/
 }
