d3c97503b22d1e133ac52316de0bf5aa534bfe51
devcluchi <luis.boehme@gmx.net>
2019-02-07 15:18:17 +0100

    added  chat

--- app/src/main/java/eulberg/datingapp/ChatAdapter.java
+++ app/src/main/java/eulberg/datingapp/ChatAdapter.java
@@ -2,6 +2,7 @@ package eulberg.datingapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
---
+++ app/src/main/java/eulberg/datingapp/Message.java
@@ -0,0 +1,111 @@
+package eulberg.datingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Message extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference databaseReference;
+
+    ImageButton sendButton;
+    EditText message;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        //TODO Toolbar mit einbauen
+
+        profile_image = findViewById(R.id.messageImage);
+        username = findViewById(R.id.messageUsername);
+        sendButton = findViewById(R.id.sendButton);
+        message = findViewById(R.id.messageText);
+
+        intent = getIntent();
+        final String userID = intent.getStringExtra("userID");
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(userID);
+        getIncomingIntent();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                //TODO Profilbild-Abfrage
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        }
+
+        );
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = message.getText().toString();
+                if(!msg.equals("")){
+                    sendMessage(firebaseUser.getUid(),userID,msg);
+                }else{
+                    Toast.makeText(Message.this,"Tippe eine Nachricht ein",Toast.LENGTH_SHORT).show();
+                }
+                message.setText("");
+            }
+        });
+    }
+
+    private void sendMessage(String sender, String reciever,String message){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("reciever", reciever);
+        hashMap.put("message", message);
+
+        reference.child("chats").push().setValue(hashMap);
+
+    }
+
+    private void getIncomingIntent(){
+        if(getIntent().hasExtra("userID")){
+            String chatUser = getIntent().getStringExtra("userID");
+        }
+    }
+
+
+
+
+}
