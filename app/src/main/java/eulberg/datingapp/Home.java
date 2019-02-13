0e07016034f19601297a4e4acc2e983d0683b970
devcluchi <luis.boehme@gmx.net>
2019-02-13 23:19:04 +0100

    profile fragment as default fragment

--- app/src/main/java/eulberg/datingapp/Home.java
+++ app/src/main/java/eulberg/datingapp/Home.java
@@ -85,7 +85,7 @@ public class Home extends AppCompatActivity implements InterstitialListener {
         BottomNavigationView nav = findViewById(R.id.navigation_bar);
         nav.setOnNavigationItemSelectedListener(navListener);
         //Default fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, chatFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, profileFragment).commit();
 
 
         checkIfGPSIsEnabled();
